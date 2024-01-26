c778dc62f16d2077327ac53b5a16cdd5e6315a89
reemdalab <78297370+reemdalab@users.noreply.github.com>
2024-01-27 00:50:48 +0300

    first repo

---
+++ myscript.js
@@ -0,0 +1,18 @@
+function sum(nums, target) {
+    const numIndices = {};
+
+    for (let i = 0; i < nums.length; i++) {
+        const complement = target - nums[i];
+
+        if (numIndices.hasOwnProperty(complement)) {
+            return [numIndices[complement], i];
+        }
+
+        numIndices[nums[i]] = i;
+    }
+}
+
+const nums = [2, 9, 11, 15];
+const target = 20;
+const result = sum(nums, target);
+console.log(result);
